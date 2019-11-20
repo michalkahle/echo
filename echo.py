4df0860a8d9ca37c0dfda22810a465a9f3e3d669
Michal Kahle <michalkahle@gmail.com>
2019-11-20 15:25:42 +0100

    improvements

--- echo.py
+++ echo.py
@@ -7,7 +7,8 @@ import re
 import os
 import xml.etree.ElementTree as ET
 
-_rows = np.array([chr(x) for x in range(65, 91)] + ['A' + chr(x) for x in range(65, 71)])
+_rows = np.array([chr(x) for x in range(65, 91)]
+        + ['A' + chr(x) for x in range(65, 71)])
 
 def welln2well(wells, form):
     form = int(form)
@@ -54,7 +55,16 @@ well2welln_1536 = partial(well2welln, form=1536)
 def plot_picklist(picklist, form, fill='v', show='target', **kwargs):
     if type(picklist) == str:
         picklist = read_picklist(picklist)
-    plate = 't_plate' if show == 'target' else 's_plate'
+    if show == 'source':
+        plate = 's_plate'
+    elif show == 'target':
+        plate = 't_plate'
+    elif show == 'sourcesum':
+        picklist = picklist.groupby(['s_well', 's_plate'])['v'].sum().reset_index()
+        plate, show = 's_plate', 'source'
+    elif show == 'targetsum':
+        picklist = picklist.groupby(['t_well', 't_plate'])['v'].sum().reset_index()
+        plate, show = 't_plate', 'target'
     for barcode, pl in picklist.groupby(plate):
         gg = plot_plate(pl, form, fill=fill, alpha=.5, show=show, **kwargs)
         gg += p9.ggtitle(barcode)
@@ -78,7 +88,10 @@ def read_survey(fn):
         for child in root if float(child.attrib['vl']) > 0.0]
     df = pd.DataFrame(ll)
     df['welln'] = well2welln(df['well'], form)
-    df['barcode'] = root.attrib['barcode']
+    if root.attrib['barcode'] != 'UnknownBarCode':
+        df['plate'] = root.attrib['barcode']
+    else:
+        df['plate'] = None
     df['format'] = form
     return df
 
@@ -87,20 +100,19 @@ def plot_survey_dir(directory):
         if fn.lower().endswith('_platesurvey.xml'):
             plot_survey(os.path.join(directory, fn))
 
-def plot_survey(survey):
+def plot_survey(survey, limits=None):
     if type(survey) == str:
         survey = read_survey(survey)
     form = survey['format'].iloc[0]
-    cl = {1536:[0,6], 384:[0,None]}[form]
+    limits = {1536:[0,6], 384:[0,None]}[form] if not limits else limits
 
-    gg = plot_plate(survey, form=form, fill='v', discrete=False,
-                    color_limits=cl)
+    gg = plot_plate(survey, form=form, fill='v', discrete=False, limits=limits)
     if 'barcode' in survey.columns:
-        gg += p9.ggtitle(survey.iloc[0]['barcode'])
+        gg += p9.ggtitle(survey.iloc[0]['plate'])
     gg.draw()
 
 def plot_plate(df, form, fill='v', alpha=1, discrete=True, show='target',
-                color_limits=[None, None]):
+                limits=[None, None]):
     df = df.copy()
     if form not in [96, 384, 1536]:
         raise ValueError('Only forms 96, 384 and 1536 supported.')
@@ -118,7 +130,7 @@ def plot_plate(df, form, fill='v', alpha=1, discrete=True, show='target',
         welln = df['welln']
     df['col'] = welln % n_cols + 1
     df['row'] = welln // n_cols + 1
-    size = (6, 4) if form < 1536 else (12,8)
+    size = (6, 4) if form < 1536 else (8,6)
     labels = _rows[:n_rows]
     if discrete:
         fill = 'factor(%s)' % fill
@@ -145,10 +157,10 @@ def plot_plate(df, form, fill='v', alpha=1, discrete=True, show='target',
     if discrete:
         gg += p9.scale_fill_discrete(l=.4, s=1, color_space='hls')
     else:
-        gg += p9.scale_fill_cmap(name='jet', limits=color_limits)
+        gg += p9.scale_fill_cmap(name='jet', limits=limits)
     return gg
 
-def read_report_csv(fn):
+def read_log_csv(fn):
     start = 9
     with open(fn) as file:
         for nn, line in enumerate(file):
@@ -171,7 +183,8 @@ def read_report_csv(fn):
         'VolumeTransferred (nL)':'v',
         'CurrentFluidThickness (mm)':'h_after'}, axis=1)
     # return df
-    return df[['s_well',  's_row', 's_col', 't_well', 't_row', 't_col', 'skip', 'composition', 'h_before', 'v', 'h_after']]
+    return df[['s_well',  's_row', 's_col', 't_well', 't_row', 't_col', 'skip',
+            'composition', 'h_before', 'v', 'h_after']]
 
 def check_log_dir(directory, fix=False):
     nn, nf = 0, 0
@@ -187,10 +200,13 @@ def check_log_dir(directory, fix=False):
                 for fail in failed:
                     nf += 1
                     if not fix:
-                        print('{s_plate} {s_well} -> {t_plate}/{t_well} [{v:>4}nl] {reason}'.format(**fail))
-    if fix:
-        write_picklist(pd.DataFrame(ll), os.path.join(directory, 'fix_picklist.csv'))
+                        print('{s_plate} {s_well} -> {t_plate}/{t_well} '
+                            '[{v:>4}nl] {reason}'.format(**fail))
     print('%i logs checked, %i failures.' % (nn, nf))
+    if fix:
+        fix_fn = os.path.join(directory, 'fix_picklist.csv')
+        write_picklist(pd.DataFrame(ll), fix_fn)
+        print('Fix picklist created: ', fix_fn)
 
 def check_log(fn):
     ll = []
@@ -209,6 +225,39 @@ def check_log(fn):
             'reason': el.attrib['reason'],
         })
     return ll if len(ll) > 0 else None
+
+def check_source_volume(picklist, survey, plate=None):
+    if type(picklist) == str:
+        picklist = read_picklist(picklist)
+    if type(survey) == str:
+        survey = read_survey(survey)
+    if not plate:
+        pl_plates = picklist['s_plate'].unique()
+        pl_plate = pl_plates[0] if pl_plates.shape[0] == 1 else None
+        sv_plate = survey['plate'][0]
+        if pl_plate and not sv_plate:
+            plate = pl_plate
+        elif not pl_plate and sv_plate:
+            plate = sv_plate
+        elif pl_plate and sv_plate and pl_plate == sv_plate:
+            plate = sv_plate
+        elif pl_plate and sv_plate and pl_plate != sv_plate:
+            raise ValueError('Surveyed plate is not on picklist.')
+        elif not pl_plate and not sv_plate:
+            raise ValueError('It is not clear which plate to check.')
+
+
+
+
+
+    pl = (picklist.groupby(['s_well', 's_plate'])['v']
+        .sum().reset_index()
+        .rename({'s_well': 'well', 'v':'vp'}, axis=1))
+    res = survey.merge(pl)
+    res['vf'] = res['v'] - res['vp'] / 1000
+    form = survey['format'][0]
+    gg = plot_plate(res, form=form, fill='vf', discrete=False, limits=[0,1])
+    return gg + p9.ggtitle(plate)
 
 if __name__ == '__main__':
     pl3 = pd.DataFrame({'t_well':['A1', 'B2', 'H12'], 'v':[1,2,3]})
